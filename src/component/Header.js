73d9cd2deb433355f12de9fd9e5d2622197b6577
igormandic <igormandic996@gmail.com>
2020-04-30 22:59:45 +0200

    Sredjen navBar

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,20 +2,38 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/Header.css';
 import { Link } from 'react-router-dom';
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class Header extends Component {
-render(){
+  constructor(init){
+    super(init);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange({target}){
+    let nb = document.getElementById('NB_response');
+    if (target.checked){
+      nb.style.display = "block";
+    } else {
+      nb.style.display = "none";
+    }
+  }
+
+  exitNb() { 
+    let nb = document.getElementById('NB_response');
+    nb.style.display = "none";
+    let cb = document.getElementById('check');
+    cb.checked = '';
+
+  }
+
+  render(){
 
   return (
     <div>
       <div className="header">
 
         <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" />
-
-        {/* <input type="checkbox" id="check"/>
-        <label for="check" class="checkbtn">
-        <img src="/fa_bars.png" className="img_bars" alt="fa_bars"></img>
-        </label> */}
 
         <div className="logo_header">
           <button >
@@ -26,8 +44,9 @@ render(){
         </div>
 
         <div className="tri_crte">
-          <input type="checkbox" id="check"/>
-          <label for="check" class="checkbtn">
+          <label htmlFor="check" className="checkbtn">
+          <input type="checkbox" id="check" onClick={this.handleChange}
+                        defaultChecked={this.props.complete}/>
             <img src="/fa_bars.png" className="img_bars" alt="fa_bars"></img>
           </label>
         </div>
@@ -69,7 +88,9 @@ render(){
 
       </div>
 
-      <div className="NB_response">
+      <div className="NB_response" id="NB_response">
+      <button className="dugmeNavBar" onClick={this.exitNb}><FontAwesomeIcon icon={faTimes} color="white" size="3x"/></button>
+
           <ul>
             <li><a href="/"> O nama </a></li>
             <li><a href="/#"> Projekti </a>
